a6778c293309e911c0917b8b805a90bbfabdf035
jassilearg <jassilearg@gmail.com>
2025-12-14 20:49:13 -0300

    test(favoritos): adiciona teste de favoritar time

---
+++ cypress/e2e/favoritos.cy.js
@@ -0,0 +1,18 @@
+import HomePage from '../pages/home.page';
+import FavoritosPage from '../pages/favoritos.page';
+
+describe('Favoritos — Favoritar time', () => {
+  beforeEach(() => {
+    HomePage.open();
+    cy.login('usuario');
+    cy.intercept('GET', '**/user/profile/**').as('getProfile');
+    cy.wait('@getProfile');
+    cy.visit('/favoritos');
+    cy.url({ timeout: 10000 }).should('include', '/favoritos');
+    FavoritosPage.esperarPaginaPronta();
+  });
+
+  it('Deve favoritar um time com sucesso', () => {
+    FavoritosPage.favoritarPrimeiroTime();
+  });
+});
---
+++ cypress/pages/favoritos.page.js
@@ -0,0 +1,50 @@
+class FavoritosPage {
+    seletores = {
+      botaoFavoritarTime: '[data-cy="btn-favorite-team"]',
+      botaoConfirmarTimes: '[data-cy="btn-submit-teams"]',
+      cardTimeFavorito: '.css-1ubyim7',
+      modalSelecaoTimes: '[role="dialog"]',
+    };
+
+    esperarPaginaPronta() {
+      cy.url({ timeout: 10000 }).should('include', '/favoritos');
+        cy.get(this.seletores.botaoFavoritarTime, { timeout: 10000 })
+        .should('exist');
+    }
+
+    clicarEmFavoritarTime() {
+      cy.get(this.seletores.botaoFavoritarTime, { timeout: 10000 })
+        .should('exist')
+        .scrollIntoView()
+        .click({ force: true });
+    }
+    adicionarPrimeiroTimeDaLista() {
+        cy.get('button')
+          .contains(/^Add$/)
+          .first()
+          .should('exist')
+          .click({ force: true });
+    }
+
+    confirmarSelecaoDeTimes() {
+      cy.get(this.seletores.botaoConfirmarTimes, { timeout: 10000 })
+        .should('exist')
+        .scrollIntoView()
+        .click({ force: true });
+    }
+  
+    validarTimeFavoritado() {
+      cy.get(this.seletores.cardTimeFavorito, { timeout: 10000 })
+        .first()
+        .should('exist');
+    }
+
+    favoritarPrimeiroTime() {
+      this.clicarEmFavoritarTime();
+      this.adicionarPrimeiroTimeDaLista();
+      this.confirmarSelecaoDeTimes();
+      this.validarTimeFavoritado();
+    }
+}
+  
+export default new FavoritosPage();
