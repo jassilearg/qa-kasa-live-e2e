f3771e0a0b19c1c63fef5270247b39426ea1943c
jassilearg <jassilearg@gmail.com>
2025-12-16 19:00:42 -0300

    fix: faz ignorar a exceção do javascript

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,7 @@
 import './commands'
+Cypress.on('uncaught:exception', (err) => {
+    if (err.message.includes('reading \'document\'')) {
+      return false; // impede o Cypress de falhar o teste
+    }
+  });
+  
